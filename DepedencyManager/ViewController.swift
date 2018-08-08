91a2dadb024fa9c735b27020d47090ac47bb683b
Ricky Adi Kuncoro <riq.adi@gmail.com>
2018-08-08 15:44:47 +0700

    asdasd

---
+++ DepedencyManager/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DepedencyManager
+//
+//  Created by Ricky Adi Kuncoro on 08/08/18.
+//  Copyright © 2018 est. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SVProgressHUD.show()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
